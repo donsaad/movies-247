608af2cc1590e8efeb9b79d89d23155f8fc5f6aa
Mohamed Saad <mohamedsaadamin.cis@gmail.com>
2015-12-17 14:07:52 +0200

    Got data from server

--- app/src/main/java/com/donsaad/movies247/MainActivity.java
+++ app/src/main/java/com/donsaad/movies247/MainActivity.java
@@ -1,13 +1,116 @@
 package com.donsaad.movies247;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.GridView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView mGridView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        new FetchMoviesTask().execute("KEY_HERE"); // replace with the key
+
     }
+
+    private void init() {
+        mGridView = (GridView) findViewById(R.id.grid_movies);
+    }
+
+    /**
+     * AsyncTask parameter is the API key
+     * and it return the JSON response as a String
+     */
+    public class FetchMoviesTask extends AsyncTask<String, Void, String> {
+
+        private final String LOG_TAG = FetchMoviesTask.class.getSimpleName();
+
+        @Override
+        protected String doInBackground(String... params) {
+            // verifying parameters
+            if(params.length == 0) {
+                return  null;
+            }
+            HttpURLConnection httpURLConnection = null;
+            BufferedReader reader = null;
+
+            try {
+                String baseUrl = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=";
+                baseUrl += params[0];
+                URL url = new URL(baseUrl);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("GET");
+                httpURLConnection.connect();
+
+                InputStream inputStream = httpURLConnection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuffer buffer = new StringBuffer();
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if(buffer.length() == 0) {
+                    // no data
+                    return null;
+                }
+
+                return buffer.toString();
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Connection Error: ",e);
+            }
+            finally {
+                if (httpURLConnection != null) {
+                    httpURLConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(LOG_TAG, "Error closing stream", e);
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if(s != null) {
+                try {
+                    JSONArray movies = new JSONObject(s).getJSONArray("results");
+                    // JSON respone test
+                    Log.i(LOG_TAG, movies.getJSONObject(0).toString());
+                } catch (JSONException e) {
+                    Log.e(LOG_TAG, "Error parsing json: ", e);
+                }
+            }
+        } // end of onPostExecute
+
+    } // end of FetchMoviesTask
+
 }
