f0bd8d5bbe5ac1778a40bd3b881430810c7b2b21
Mohamed Saad <mohamedsaadamin.cis@gmail.com>
2016-01-07 15:41:58 +0200

    Generalized data fetching

--- app/src/main/java/com/donsaad/movies247/MainActivity.java
+++ app/src/main/java/com/donsaad/movies247/MainActivity.java
@@ -9,11 +9,6 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -25,13 +20,13 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnDataFetchListener {
 
     public final String LOG_TAG = MainActivity.class.getSimpleName();
 
     private static final String API_KEY = ""; // TODO: replace with your key
-    private static final String MOVIES_SORT_BY_POPULARITY = "popularity.desc";
-    private static final String MOVIES_SORT_BY_RATE = "vote_average.desc";
+    private static final String FETCH_MOVIES_BY_POPULARITY = "http://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=";
+    private static final String FETCH_MOVIES_BY_RATE = "http://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc&api_key=";
     public static final String BASE_POSTER_URL = "http://image.tmdb.org/t/p/w185";
     public static final String MOVIE_OVERVIEW_KEY = "overview";
     public static final String MOVIE_VOTE_AVG_KEY = "vote_average";
@@ -41,10 +36,7 @@ public class MainActivity extends AppCompatActivity {
     public static final String MOVIES_KEY = "results";
 
     private RecyclerView recyclerView;
-    private List<Movie> movieList;
-    private RecyclerAdapter adapter;
-    private JSONArray movies;
-    private JSONObject movie;
+    private List<Movie> moviesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,27 +45,13 @@ public class MainActivity extends AppCompatActivity {
 
         init();
         DataFetchTask dataFetchTask = new DataFetchTask();
-        dataFetchTask.setOnDataFetchListener(new OnDataFetchListener() {
-            @Override
-            public void onDataFetched(String data) {
-                MovieParser parser = new MovieParser();
-                movieList = parser.parseJson(data);
-                adapter = new RecyclerAdapter(MainActivity.this, movieList);
-                recyclerView.setAdapter(adapter);
-            }
-
-            @Override
-            public void onDataError(int errorCode) {
-
-            }
-        });
-        dataFetchTask.execute(MOVIES_SORT_BY_POPULARITY);
+        dataFetchTask.setOnDataFetchListener(this);
+        dataFetchTask.execute(FETCH_MOVIES_BY_POPULARITY);
 
     }
 
     private void init() {
-        movieList = new ArrayList<>();
-
+        moviesList = new ArrayList<>();
         recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false));
@@ -90,19 +68,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        // TODO: 1/7/2016 Enabled sort option (disabled temporarily)
         if (id == R.id.action_sort_by_popular) {
-//            new DataFetchTask().execute(MOVIES_SORT_BY_POPULARITY);
+            DataFetchTask dataFetchTask = new DataFetchTask();
+            dataFetchTask.setOnDataFetchListener(this);
+            dataFetchTask.execute(FETCH_MOVIES_BY_POPULARITY);
         } else if (id == R.id.action_sort_by_rate) {
-//            new DataFetchTask().execute(MOVIES_SORT_BY_RATE);
+            DataFetchTask dataFetchTask = new DataFetchTask();
+            dataFetchTask.setOnDataFetchListener(this);
+            dataFetchTask.execute(FETCH_MOVIES_BY_RATE);
         } else if (id == R.id.action_sort_by_fav) {
             // // TODO: 12/25/2015 fav 
         }
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onDataFetched(String data) {
+        MovieParser parser = new MovieParser();
+        moviesList = parser.parseJson(data);
+        recyclerView.setAdapter(new RecyclerAdapter(MainActivity.this, moviesList));
+    }
+
+    @Override
+    public void onDataError(int errorCode) {
+        Log.e(LOG_TAG, "Error: DataFetchTask Error Code: " + errorCode);
+    }
+
     /**
-     * AsyncTask parameter is the API key
+     * AsyncTask parameter is the endpoint to fetch data from it
      * and it return the JSON response as a String
      */
     public class DataFetchTask extends AsyncTask<String, Void, String> {
@@ -123,9 +116,7 @@ public class MainActivity extends AppCompatActivity {
             BufferedReader reader = null;
 
             try {
-                String baseUrl = "http://api.themoviedb.org/3/discover/movie?sort_by=" +
-                        params[0] + "&api_key=" + API_KEY;
-                URL url = new URL(baseUrl);
+                URL url = new URL(params[0] + API_KEY);
                 httpURLConnection = (HttpURLConnection) url.openConnection();
                 httpURLConnection.setRequestMethod("GET");
                 httpURLConnection.connect();
@@ -171,7 +162,6 @@ public class MainActivity extends AppCompatActivity {
                 onDataFetchListener.onDataFetched(s);
             } else {
                 onDataFetchListener.onDataError(0);
-                Log.e(LOG_TAG, "Error: DataFetchTask null result.");
             }
 
         } // end of onPostExecute
